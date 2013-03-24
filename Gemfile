1441bf4d28880eef46bd3c75d9fa4e62d4d1bc02
vergasta <vergasta@gmail.com>
2013-03-24 17:29:52 -0300

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails', '3.2.13'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
@@ -19,7 +22,11 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
-end
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
+  gem 'wdm', '0.1'
+end 
 
 group :production do
   gem 'pg', '0.12.2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,11 +12,14 @@ describe "Static pages" do
 	it "should have the right title" do
 	  visit '/static_pages/home'
 	  page.should have_selector('title',
-	                    :text => "Ruby on Rails Tutorial Sample App | Home")
+	                    :text => "Ruby on Rails Tutorial Sample App")
 	end
 
-
+  it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
   end
+
 
   describe "Help page" do
 
@@ -44,7 +47,21 @@ describe "Static pages" do
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
-
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+
+end  
 
 end
